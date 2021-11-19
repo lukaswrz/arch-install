241da34aa1761edbba796db293244514c5a2d0b9
lukaswrz <lukas@wrz.one>
2021-11-19 22:40:31 +0100

    retab

--- arch-install.bash
+++ arch-install.bash
@@ -5,21 +5,21 @@ set -eu
 shopt -s extglob globstar nullglob
 
 function escape {
-	for a in "$@"; do
-		printf '%s\n' "${a@Q}"
-	done
+  for a in "$@"; do
+    printf '%s\n' "${a@Q}"
+  done
 }
 
 function unescape {
-	for a in "$@"; do
-		eval "cat <<< $a"
-	done
+  for a in "$@"; do
+    eval "cat <<< $a"
+  done
 }
 
 if efivar -l >/dev/null 2>&1; then
-	bios='uefi'
+  bios='uefi'
 else
-	bios='legacy'
+  bios='legacy'
 fi
 
 pacman -Sy --noconfirm fzy jq
@@ -36,17 +36,17 @@ fdisk -l
 block_devices=()
 
 for f in /dev/**/*; do
-	if [[ -b "$f" ]]; then
-		block_devices+=("$f")
-	fi
+  if [[ -b "$f" ]]; then
+    block_devices+=("$f")
+  fi
 done
 
 block_device=$(
-	unescape "$(
-		for dev in "${block_devices[@]}"; do
-			printf '%s\n' "$(escape "$dev")"
-		done | fzy -p 'enter block device (e.g. /dev/sda)> '
-	)"
+  unescape "$(
+    for dev in "${block_devices[@]}"; do
+      printf '%s\n' "$(escape "$dev")"
+    done | fzy -p 'enter block device (e.g. /dev/sda)> '
+  )"
 )
 
 clear
@@ -54,18 +54,18 @@ clear
 zones=(/usr/share/zoneinfo/**/*)
 
 zone=$(
-	unescape "$(
-		for zone in "${zones[@]}"; do
-			printf '%s\n' "$(escape "$zone")"
-		done | fzy -p 'enter zone information> '
-	)"
+  unescape "$(
+    for zone in "${zones[@]}"; do
+      printf '%s\n' "$(escape "$zone")"
+    done | fzy -p 'enter zone information> '
+  )"
 )
 
 clear
 
 
 if [[ $bios == 'uefi' ]]; then
-	read -r -e -p 'enter bootloader id> ' bootloader_id
+  read -r -e -p 'enter bootloader id> ' bootloader_id
 fi
 
 clear
@@ -73,73 +73,73 @@ clear
 read -r -e -p 'enter hostname> ' hostname
 read -r -e -p 'enter username> ' username
 while true; do
-	read -r -s -p 'enter password> ' password
-	printf '\n'
-	read -r -s -p 're-enter password> ' repassword
-	printf '\n'
-	if [[ $password == "$repassword" ]]; then
-		hashed_password=$(openssl passwd -1 -stdin <<< "$password")
-		break
-	fi
-	clear
+  read -r -s -p 'enter password> ' password
+  printf '\n'
+  read -r -s -p 're-enter password> ' repassword
+  printf '\n'
+  if [[ $password == "$repassword" ]]; then
+    hashed_password=$(openssl passwd -1 -stdin <<< "$password")
+    break
+  fi
+  clear
 done
 
 sgdisk --zap-all "$block_device"
 
 case "$bios" in
-	'uefi')
-		# efi part
-		sgdisk --new=1:0:+512M "$block_device"
-		# root
-		sgdisk --new=2:0:0 "$block_device"
-		;;
-	'legacy')
-		# root
-		parted "$block_device" mklabel msdos mkpart primary 0% 100%
-		;;
+  'uefi')
+    # efi part
+    sgdisk --new=1:0:+512M "$block_device"
+    # root
+    sgdisk --new=2:0:0 "$block_device"
+    ;;
+  'legacy')
+    # root
+    parted "$block_device" mklabel msdos mkpart primary 0% 100%
+    ;;
 esac
 
 partitions=()
 query=$(sfdisk -J "$block_device")
 for k in $(jq '.partitiontable.partitions | keys | .[]' <<< "$query"); do
-	partitions+=("$(jq --argjson k "$k" -r '.partitiontable.partitions | .[$k] | .node' <<< "$query")")
+  partitions+=("$(jq --argjson k "$k" -r '.partitiontable.partitions | .[$k] | .node' <<< "$query")")
 done
 
 case "$bios" in
-	'uefi')
-		efi_fs="${partitions[0]}"
-		root_fs="${partitions[1]}"
-		yes | mkfs.fat -F32 "$efi_fs"
-		yes | mkfs.ext4 "$root_fs"
-		;;
-	'legacy')
-		root_fs="${partitions[0]}"
-		yes | mkfs.ext4 "$root_fs"
-		;;
+  'uefi')
+    efi_fs="${partitions[0]}"
+    root_fs="${partitions[1]}"
+    yes | mkfs.fat -F32 "$efi_fs"
+    yes | mkfs.ext4 "$root_fs"
+    ;;
+  'legacy')
+    root_fs="${partitions[0]}"
+    yes | mkfs.ext4 "$root_fs"
+    ;;
 esac
 
 mount "$root_fs" /mnt
 
 if [[ $bios == 'uefi' ]]; then
-	mkdir -p /mnt/boot/efi
-	mount "$efi_fs" /mnt/boot/efi
+  mkdir -p /mnt/boot/efi
+  mount "$efi_fs" /mnt/boot/efi
 fi
 
 packages=(
-	base
-	base-devel
-	linux
-	linux-firmware
-	linux-headers
-	git
-	mkinitcpio
-	networkmanager
-	neovim
-	grub
-	sudo
-	efibootmgr
-	xdg-utils
-	xdg-user-dirs
+  base
+  base-devel
+  linux
+  linux-firmware
+  linux-headers
+  git
+  mkinitcpio
+  networkmanager
+  neovim
+  grub
+  sudo
+  efibootmgr
+  xdg-utils
+  xdg-user-dirs
 )
 
 pacstrap /mnt "${packages[@]}"
@@ -149,7 +149,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 export hostname username hashed_password keymap zone bios block_device
 
 if [[ $bios == 'uefi' ]]; then
-	export bootloader_id
+  export bootloader_id
 fi
 
 arch-chroot /mnt /bin/bash <<'EOF'
@@ -167,20 +167,20 @@ printf '%s\n' 'LANG=en_US.UTF-8' > /etc/locale.conf
 printf '%s\n' "$hostname" > /etc/hostname
 
 case "$bios" in
-	'uefi')
-		grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="$bootloader_id"
-		;;
-	'legacy')
-		grub-install --target=i386-pc "$block_device"
-		;;
+  'uefi')
+    grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="$bootloader_id"
+    ;;
+  'legacy')
+    grub-install --target=i386-pc "$block_device"
+    ;;
 esac
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
 if [[ $bios == 'uefi' ]]; then
-	# magic
-	mkdir -p /boot/efi/EFI/boot
-	cp "/boot/efi/EFI/$bootloader_id/grubx64.efi" /boot/efi/EFI/boot/bootx64.efi
+  # magic
+  mkdir -p /boot/efi/EFI/boot
+  cp "/boot/efi/EFI/$bootloader_id/grubx64.efi" /boot/efi/EFI/boot/bootx64.efi
 fi
 
 useradd -m -G wheel -p "$hashed_password" -- "$username"
